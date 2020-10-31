cb0ed08082e0296c113f43320a6399d4f4b3dd86
phn <pichichi91@users.noreply.github.com>
2020-11-01 00:22:54 +0100

    Update Preview

--- src/App.js
+++ src/App.js
@@ -203,6 +203,7 @@ function App() {
 <meta property="og:title" content="FMCanvas | Movement of Tactical Roles in Football"/>
 <meta property="og:description" content="This react based WebApp, tries to replicate movement of the available Roles in Football Manager"></meta>
 <meta property="og:image" content="https://fm-canvas.vercel.app/showcase.PNG"/>
+<meta property="twitter:image" content="https://fm-canvas.vercel.app/showcase.PNG"/>
 
 
 </Helmet>
