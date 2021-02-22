dfecfdc25f487eef85d6e40834dae1b04f73c16b
phn <pichichi91@users.noreply.github.com>
2021-02-22 18:18:25 +0100

    Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,11 @@ import { Helmet } from 'react-helmet-async';
 
 const Header = () => {
     return(<Helmet>
-        <meta name="og:title" content="FMCanvas | Movement of Tactical Roles in Football"/>
-        <meta name="og:description" content="This react based WebApp, tries to replicate movement of the available Roles in Football Manager"/>
-        <meta name="twitter:title" content="FMCanvas | Movement of Tactical Roles in Football"/>
-        <meta name="twitter:description" content="This react based WebApp, tries to replicate movement of the available Roles in Football Manager"/>
-        <meta name="og:image" content="https://wp.dossis.ch/wp-content/uploads/2021/02/Unbenannt-10.png"/>
-        <meta name="twitter:image" content="https://wp.dossis.ch/wp-content/uploads/2021/02/Unbenannt-10.png"/>
-        <meta name="twitter:card" content="summary_large_image" data-react-helmet="true"></meta>
-        <meta name="og:url" content="https://fm-canvas.vercel.app" data-react-helmet="true"></meta>
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@dossis"/>
+    <meta name="twitter:title" content="FMCanvas"/>
+    <meta name="twitter:description" content="Movement of Tactical Roles in Football"/>
+    <meta name="twitter:image" content="https://wp.dossis.ch/wp-content/uploads/2021/02/Unbenannt-10.png"/>
         </Helmet>)
 }
 
