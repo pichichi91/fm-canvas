eaecfb02be59aa88fc013ab4ae88e05c7bf5a5e6
phn <pichichi91@users.noreply.github.com>
2021-02-22 16:44:46 +0100

    Update Image

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
         <meta name="og:description" content="This react based WebApp, tries to replicate movement of the available Roles in Football Manager"/>
         <meta name="twitter:title" content="FMCanvas | Movement of Tactical Roles in Football"/>
         <meta name="twitter:description" content="This react based WebApp, tries to replicate movement of the available Roles in Football Manager"/>
-        <meta name="og:image" content="https://fm-canvas.vercel.app/showcase.PNG"/>
-        <meta name="twitter:image" content="https://fm-canvas.vercel.app/showcase.PNG"/>
+        <meta name="og:image" content="https://wp.dossis.ch/wp-content/uploads/2021/02/Unbenannt-10.png"/>
+        <meta name="twitter:image" content="https://wp.dossis.ch/wp-content/uploads/2021/02/Unbenannt-10.png"/>
         <meta name="twitter:card" content="summary_large_image" data-react-helmet="true"></meta>
         <meta name="og:url" content="https://fm-canvas.vercel.app" data-react-helmet="true"></meta>
         </Helmet>)
