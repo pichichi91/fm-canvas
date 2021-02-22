c8c87204268672722f6b6995e97224b1479276d8
phn <pichichi91@users.noreply.github.com>
2021-02-22 18:37:19 +0100

    Update Twitter Card

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,17 @@ import { Helmet } from 'react-helmet-async';
 const Header = () => {
     return(<Helmet>
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="@dossis"/>
+    <meta name="og:site" content="@dossis"/>
     <meta name="twitter:title" content="FMCanvas"/>
-    <meta name="twitter:description" content="Movement of Tactical Roles in Football"/>
-    <meta name="twitter:image" content="https://wp.dossis.ch/wp-content/uploads/2021/02/Unbenannt-10.png"/>
+    <meta name="og:description" content="Movement of Tactical Roles in Football"/>
+    <meta name="og:image" content="https://wp.dossis.ch/wp-content/uploads/2021/02/Unbenannt-10.png"/>
+
+    <meta name="og:title" content="FMCanvas"/>
+
+
+
+
+      
         </Helmet>)
 }
 
